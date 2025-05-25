50edd11f6e836cf3a402840d89acfa46673c3392
Fasih Javed <fasihjaved@Fasihs-Mac-mini.local>
2025-05-24 21:39:47 -0500

    working on it still

--- compiler.cpp
+++ compiler.cpp
@@ -5,6 +5,28 @@
 #include <string>
 #include <iostream>
 
+struct Parser {
+  Token current;
+  Token previous;
+};
+
+Parser parser;
+
+static void errorAtCurrent(const std::string& message) {
+  errorAt(&parser.current, message);
+}
+
+static void advance() {
+  parser.previous = parser.current;
+
+  while (true) {
+    parset.current = scanToken();
+    if (parset.current.type != TOKEN_ERROR) break;
+
+    errorAtCurrent(parser.current.start);
+  }
+}
+
 bool compile(const std::string source, Chunk* chunk) {
   initScanner(source);
   advance();
